855a438f196ffe986c840eee8db26cf5fbcadde7
VitalyW <vitzeev1@gmail.com>
2017-12-20 12:04:28 -0600

    Implicit and Explicit waits

--- java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFirstTest.java
@@ -2,6 +2,7 @@ package ru.stqa.training.selenium;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -11,7 +12,8 @@ public class MyFirstTest extends TestBase {
   public void myFirstTest() {
     driver.get("https://www.google.com/");
     driver.findElement(By.name("q")).sendKeys("webdriver");
-    driver.findElement(By.name("btnK")).click();
+    WebElement btnG = driver.findElement(By.name("btnK"));
+    btnG.click();
     wait.until(titleIs("webdriver - Google Search"));
   }
 
--- java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
+++ java-example/src/test/java/ru/stqa/training/selenium/TestBase.java
@@ -7,11 +7,28 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.NoSuchElementException;
+import java.util.concurrent.TimeUnit;
+
 public class TestBase {
 
   public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
   public WebDriver driver;
   public WebDriverWait wait;
+
+  public boolean isElementPresent(By locator) {
+    try {
+      wait.until((WebDriver d) -> d.findElement(locator)); // excplicit wait
+//      driver.findElement(locator);
+      return true;
+    } catch (TimeoutException | NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  public boolean areElementsPresent(By locator) {
+    return driver.findElements(locator).size() > 0;
+  }
 
   @Before
   public void start() {
@@ -20,8 +37,8 @@ public class TestBase {
       wait = new WebDriverWait(driver, 10);
       return;
     }
-
     driver = new ChromeDriver();
+//    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);  //implicit wait
     tlDriver.set(driver);
     wait = new WebDriverWait(driver, 10);
 
